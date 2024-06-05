e0c10b71b354a897f00502792d6468c06a75a767
yymin1022 <yymin1022@gmail.com>
2024-06-05 20:37:52 +0900

    Update MainActivity : Added Service Start/Stop Listener

--- app/src/main/java/com/caucapstone/peeper/Activity/MainActivity.kt
+++ app/src/main/java/com/caucapstone/peeper/Activity/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.caucapstone.peeper.Activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.caucapstone.peeper.R
+import com.caucapstone.peeper.Service.BluetoothService
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +20,18 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnStart = findViewById<MaterialButton>(R.id.main_btn_start)
+        val btnStop = findViewById<MaterialButton>(R.id.main_btn_stop)
+        btnStart.setOnClickListener(btnListener)
+        btnStop.setOnClickListener(btnListener)
+    }
+
+    private val btnListener = View.OnClickListener { btn ->
+        when(btn.id) {
+            R.id.main_btn_start -> startService(Intent(this, BluetoothService::class.java))
+            R.id.main_btn_stop -> stopService(Intent(this, BluetoothService::class.java))
         }
     }
 }
