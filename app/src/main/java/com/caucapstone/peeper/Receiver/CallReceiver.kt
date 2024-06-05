5c6591c2d76b8d7130fd8a61d336f9ffa9e8027e
yymin1022 <yymin1022@gmail.com>
2024-06-05 22:00:10 +0900

    Added CallReceiver Class

---
+++ app/src/main/java/com/caucapstone/peeper/Receiver/CallReceiver.kt
@@ -0,0 +1,11 @@
+package com.caucapstone.peeper.Receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class CallReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
