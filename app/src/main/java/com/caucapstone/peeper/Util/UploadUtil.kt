dcfe0511f311fd9552fc2d2edf891e39a5e09b92
yymin1022 <yymin1022@gmail.com>
2024-06-07 02:02:08 +0900

    Update UploadUtil : Added Socket Init/Close Functions

--- app/src/main/java/com/caucapstone/peeper/Util/UploadUtil.kt
+++ app/src/main/java/com/caucapstone/peeper/Util/UploadUtil.kt
@@ -1,18 +1,37 @@
 package com.caucapstone.peeper.Util
 
+import com.caucapstone.peeper.BuildConfig
 import java.io.OutputStream
 import java.net.Socket
 
 object UploadUtil {
+    private const val SERVER_PORT = BuildConfig.SERVER_PORT
+    private const val SERVER_URL = BuildConfig.SERVER_URL
     private var serverSocket: Socket? = null
     private var serverStream: OutputStream? = null
 
     fun initSocket() {
-        
+        if(serverSocket == null || serverSocket!!.isClosed) {
+            serverSocket = Socket(SERVER_URL, SERVER_PORT.toInt())
+        }
+
+        if(serverStream == null) {
+            serverStream = serverSocket!!.getOutputStream()
+        }
     }
 
     fun closeSocket() {
+        if(serverStream != null) {
+            serverStream!!.close()
+            serverStream = null
+        }
 
+        if(serverSocket != null) {
+            if(serverSocket!!.isClosed == false) {
+                serverSocket!!.close()
+            }
+            serverSocket = null
+        }
     }
 
     fun uploadFile() {
