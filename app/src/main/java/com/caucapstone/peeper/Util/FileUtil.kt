632456bdc8c55d8fc53aadb5582724945d16a59c
yymin1022 <yymin1022@gmail.com>
2024-06-08 05:26:03 +0900

    Update FileUtil : Added Internal Function

--- app/src/main/java/com/caucapstone/peeper/Util/FileUtil.kt
+++ app/src/main/java/com/caucapstone/peeper/Util/FileUtil.kt
@@ -1,5 +1,7 @@
 package com.caucapstone.peeper.Util
 
+import java.util.Locale
+
 object FileUtil {
     private var fileCounter = 1
 
@@ -11,8 +13,8 @@ object FileUtil {
 
     }
 
-    private fun getFileName(uid: String) {
-        
+    private fun getFileName(uid: String): String {
+        return String.format(Locale.getDefault(), "%s-%d.wav", uid, fileCounter)
     }
 
     private fun writeFileHeader(filename: String) {
