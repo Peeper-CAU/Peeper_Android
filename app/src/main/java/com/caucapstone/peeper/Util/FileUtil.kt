67091046129852ef500cc05d10c252cda2eab8f9
yymin1022 <yymin1022@gmail.com>
2024-06-08 05:25:09 +0900

    Update FileUtil : Added Basic Functions

--- app/src/main/java/com/caucapstone/peeper/Util/FileUtil.kt
+++ app/src/main/java/com/caucapstone/peeper/Util/FileUtil.kt
@@ -1,4 +1,21 @@
 package com.caucapstone.peeper.Util
 
 object FileUtil {
+    private var fileCounter = 1
+
+    fun initFile(uid: String) {
+
+    }
+
+    fun closeFile(uid: String) {
+
+    }
+
+    private fun getFileName(uid: String) {
+        
+    }
+
+    private fun writeFileHeader(filename: String) {
+
+    }
 }
