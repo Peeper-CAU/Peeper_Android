b9efd2c319547af160d218a207881a5ced34aad8
yymin1022 <yymin1022@gmail.com>
2024-06-08 19:06:31 +0900

    Update FileUtil : Added WAV Header Writing Logic

--- app/src/main/java/com/caucapstone/peeper/Util/FileUtil.kt
+++ app/src/main/java/com/caucapstone/peeper/Util/FileUtil.kt
@@ -27,7 +27,7 @@ object FileUtil {
 
     fun closeFile(uid: String) {
         Log.i("Uploadutil", "Closing File")
-        if(fileStream != null){
+        if(fileStream != null) {
             UploadUtil.uploadFile(getFileFullPath(uid))
             fileCounter++
             fileStream!!.close()
@@ -49,13 +49,79 @@ object FileUtil {
     }
 
     private fun writeFileData() {
-        if(fileStream != null){
+        if(fileStream != null) {
             fileStream!!.write(fileBuffer)
             fileStream!!.flush()
         }
     }
 
-    private fun writeFileHeader(filename: String) {
-
+    private fun writeFileheaderBuffer() {
+        if(fileStream != null) {
+            val sampleRate = 16000.toLong() // 16000Hz
+            val channels = 1                // Mono Channel
+            val bitsPerSample = 8           // PCM Bit Size
+            val byteRate = sampleRate * channels * bitsPerSample / 8
+            val blockAlign = channels * bitsPerSample / 8
+            
+            val headerBuffer = ByteArray(44)
+            // 1. RIFF
+            headerBuffer[0] = 'R'.code.toByte()
+            headerBuffer[1] = 'I'.code.toByte()
+            headerBuffer[2] = 'F'.code.toByte()
+            headerBuffer[3] = 'F'.code.toByte()
+            // 2. File Size
+            headerBuffer[4] = ((fileBuffer.size + 36).toLong() and 0xffL).toByte()
+            headerBuffer[5] = ((fileBuffer.size + 36).toLong() shr 8 and 0xffL).toByte()
+            headerBuffer[6] = ((fileBuffer.size + 36).toLong() shr 16 and 0xffL).toByte()
+            headerBuffer[7] = ((fileBuffer.size + 36).toLong() shr 24 and 0xffL).toByte()
+            // 3. WAVE
+            headerBuffer[8] = 'W'.code.toByte()
+            headerBuffer[9] = 'A'.code.toByte()
+            headerBuffer[10] = 'V'.code.toByte()
+            headerBuffer[11] = 'E'.code.toByte()
+            // 4. 'fmt '
+            headerBuffer[12] = 'f'.code.toByte()
+            headerBuffer[13] = 'm'.code.toByte()
+            headerBuffer[14] = 't'.code.toByte()
+            headerBuffer[15] = ' '.code.toByte()
+            // 5. Fixed Value 16
+            headerBuffer[16] = 16
+            headerBuffer[17] = 0
+            headerBuffer[18] = 0
+            headerBuffer[19] = 0
+            // 6. Data Format : PCM(1)
+            headerBuffer[20] = 1
+            headerBuffer[21] = 0
+            // 7. Channel Size
+            headerBuffer[22] = channels.toByte()
+            headerBuffer[23] = 0
+            // 8. Sample Rate
+            headerBuffer[24] = (sampleRate and 0xffL).toByte()
+            headerBuffer[25] = (sampleRate shr 8 and 0xffL).toByte()
+            headerBuffer[26] = (sampleRate shr 16 and 0xffL).toByte()
+            headerBuffer[27] = (sampleRate shr 24 and 0xffL).toByte()
+            // 9. Byte Rate
+            headerBuffer[28] = (byteRate and 0xffL).toByte()
+            headerBuffer[29] = (byteRate shr 8 and 0xffL).toByte()
+            headerBuffer[30] = (byteRate shr 16 and 0xffL).toByte()
+            headerBuffer[31] = (byteRate shr 24 and 0xffL).toByte()
+            // 10. Block Size
+            headerBuffer[32] = blockAlign.toByte()
+            headerBuffer[33] = 0
+            // 11. Bit Depth
+            headerBuffer[34] = bitsPerSample.toByte()
+            headerBuffer[35] = 0
+            // 12.'data'
+            headerBuffer[36] = 'd'.code.toByte()
+            headerBuffer[37] = 'a'.code.toByte()
+            headerBuffer[38] = 't'.code.toByte()
+            headerBuffer[39] = 'a'.code.toByte()
+            // 13. PCM Data Size(File Size - Header Size)
+            headerBuffer[40] = (fileBuffer.size.toLong() and 0xffL).toByte()
+            headerBuffer[41] = (fileBuffer.size.toLong() shr 8 and 0xffL).toByte()
+            headerBuffer[42] = (fileBuffer.size.toLong() shr 16 and 0xffL).toByte()
+            headerBuffer[43] = (fileBuffer.size.toLong() shr 24 and 0xffL).toByte()
+            fileStream!!.write(headerBuffer, 0, 44)
+        }
     }
 }
