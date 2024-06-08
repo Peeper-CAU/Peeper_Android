9d1e5a26820ed63043f19b10dcf8c77f4a8e59bf
yymin1022 <yymin1022@gmail.com>
2024-06-08 18:42:42 +0900

    Update FileUtil : Improved FileStream Logic

--- app/src/main/java/com/caucapstone/peeper/Util/FileUtil.kt
+++ app/src/main/java/com/caucapstone/peeper/Util/FileUtil.kt
@@ -1,22 +1,30 @@
 package com.caucapstone.peeper.Util
 
+import android.content.Context
+import java.io.File
 import java.io.FileOutputStream
 import java.util.Locale
 
 object FileUtil {
     private var audioSize = 0
     private var fileCounter = 1
+    private var fileObject: File? = null
+    private var filePath: File? = null
     private var fileStream: FileOutputStream? = null
 
-    fun initFile(uid: String) {
-        if(fileStream == null){
-            fileStream = FileOutputStream(getFileName(uid))
+    fun initFile(uid: String, context: Context) {
+        if(fileObject == null) {
+            filePath = context.filesDir
+            fileObject = File(getFileFullPath(uid))
+        }
+        if(fileStream == null) {
+            fileStream = FileOutputStream(fileObject)
         }
     }
 
     fun closeFile(uid: String) {
         if(fileStream != null){
-            UploadUtil.uploadFile(getFileName(uid))
+            UploadUtil.uploadFile(getFileFullPath(uid))
             fileCounter++
             fileStream!!.close()
             fileStream = null
@@ -30,8 +38,8 @@ object FileUtil {
         }
     }
 
-    private fun getFileName(uid: String): String {
-        return String.format(Locale.getDefault(), "%s-%d.wav", uid, fileCounter)
+    private fun getFileFullPath(uid: String): String {
+        return String.format(Locale.getDefault(), "%s/%s-%d.wav", filePath!!.absolutePath, uid, fileCounter)
     }
 
     private fun writeFileHeader(filename: String) {
