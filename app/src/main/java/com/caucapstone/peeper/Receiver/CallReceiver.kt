2a45daea828538fc66ae802a4a96dd2647e6c733
yymin1022 <yymin1022@gmail.com>
2024-06-10 06:35:36 +0900

    Update CallReceiver : Start/Stop BluetoothService with Call OFFHOOK/IDLE

--- app/src/main/java/com/caucapstone/peeper/Receiver/CallReceiver.kt
+++ app/src/main/java/com/caucapstone/peeper/Receiver/CallReceiver.kt
@@ -5,10 +5,11 @@ import android.content.Context
 import android.content.Intent
 import android.telephony.TelephonyManager
 import android.util.Log
+import com.caucapstone.peeper.Service.BluetoothService
 
 class CallReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        if(intent != null) {
+        if(context != null && intent != null) {
             val intentAction = intent.action
             if(intentAction.equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
                 val intentState = intent.getStringExtra(TelephonyManager.EXTRA_STATE)
@@ -16,8 +17,12 @@ class CallReceiver: BroadcastReceiver() {
                     Log.i("CallReeiver", "Call Incoming!!")
                 } else if(intentState == TelephonyManager.EXTRA_STATE_OFFHOOK) {
                     Log.i("CallReeiver", "Call Ongoing!!")
+                    context.startService(Intent(context, BluetoothService::class.java))
+                    Log.i("CallReeiver", "Started BluetoothService!!")
                 } else if(intentState == TelephonyManager.EXTRA_STATE_IDLE) {
                     Log.i("CallReeiver", "Call Ended!!")
+                    context.stopService(Intent(context, BluetoothService::class.java))
+                    Log.i("CallReeiver", "Stopped BluetoothService!!")
                 }
             }
         }
