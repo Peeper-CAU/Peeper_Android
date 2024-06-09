8e159d1a9770f01b13b1c956fde980e34f9e188a
yymin1022 <yymin1022@gmail.com>
2024-06-10 06:28:14 +0900

    Update CallReceiver : Added Phone State Conditional Logic

--- app/src/main/java/com/caucapstone/peeper/Receiver/CallReceiver.kt
+++ app/src/main/java/com/caucapstone/peeper/Receiver/CallReceiver.kt
@@ -3,9 +3,23 @@ package com.caucapstone.peeper.Receiver
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.telephony.TelephonyManager
+import android.util.Log
 
 class CallReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        TODO("Not yet implemented")
+        if(intent != null) {
+            val intentAction = intent.action
+            if(intentAction.equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
+                val intentState = intent.getStringExtra(TelephonyManager.EXTRA_STATE)
+                if(intentState == TelephonyManager.EXTRA_STATE_RINGING) {
+                    Log.i("CallReeiver", "Call Incoming!!")
+                } else if(intentState == TelephonyManager.EXTRA_STATE_OFFHOOK) {
+                    Log.i("CallReeiver", "Call Ongoing!!")
+                } else if(intentState == TelephonyManager.EXTRA_STATE_IDLE) {
+                    Log.i("CallReeiver", "Call Ended!!")
+                }
+            }
+        }
     }
 }
