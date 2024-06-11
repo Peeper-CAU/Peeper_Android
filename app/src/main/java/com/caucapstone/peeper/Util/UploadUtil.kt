3463b6d1187c8494e966411d8ed0ea5d7545fe08
yymin1022 <yymin1022@gmail.com>
2024-06-11 22:19:28 +0900

    Update UploadUtil : Migrated to Okhttp Based Socket Connection

--- app/src/main/java/com/caucapstone/peeper/Util/UploadUtil.kt
+++ app/src/main/java/com/caucapstone/peeper/Util/UploadUtil.kt
@@ -2,72 +2,70 @@ package com.caucapstone.peeper.Util
 
 import android.util.Log
 import com.caucapstone.peeper.BuildConfig
-import java.io.DataInputStream
-import java.io.DataOutputStream
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.WebSocket
+import okhttp3.WebSocketListener
+import okio.ByteString.Companion.toByteString
+import okio.IOException
 import java.io.File
 import java.io.FileInputStream
-import java.io.OutputStream
-import java.io.OutputStreamWriter
-import java.net.Socket
-import java.nio.charset.StandardCharsets
 
 object UploadUtil {
-    private const val SERVER_PORT = BuildConfig.SERVER_PORT
     private const val SERVER_URL = BuildConfig.SERVER_URL
-    private var serverSocket: Socket? = null
-    private var serverStream: OutputStream? = null
+    private var socketClient: OkHttpClient? = null
+    private var serverSocket: WebSocket? = null
 
     fun initSocket() {
         Log.i("Uploadutil", "Initializing Socket")
-        if(serverSocket == null || serverSocket!!.isClosed) {
-            serverSocket = Socket(SERVER_URL, SERVER_PORT.toInt())
-        }
+        val request = Request.Builder().url(SERVER_URL).build()
+        val listener = object : WebSocketListener() {
+            override fun onOpen(webSocket: WebSocket, response: okhttp3.Response) {
+                Log.i("Uploadutil", "WebSocket Opened")
+            }
 
-        if(serverStream == null) {
-            serverStream = serverSocket!!.getOutputStream()
+            override fun onFailure(webSocket: WebSocket, t: Throwable, response: okhttp3.Response?) {
+                Log.e("Uploadutil", "WebSocket Failure: " + t.message)
+            }
         }
+        socketClient = OkHttpClient()
+        serverSocket = socketClient!!.newWebSocket(request, listener)
         Log.i("Uploadutil", "Initialized Socket")
     }
 
     fun closeSocket() {
         Log.i("Uploadutil", "Closing Socket")
-        if(serverStream != null) {
-            serverStream!!.close()
-            serverStream = null
-        }
-
-        if(serverSocket != null) {
-            if(serverSocket!!.isClosed == false) {
-                serverSocket!!.close()
-            }
-            serverSocket = null
-        }
+        socketClient!!.dispatcher.executorService.shutdown()
+        socketClient = null
+        serverSocket?.close(1000, "Closing WebSocket")
+        serverSocket = null
         Log.i("Uploadutil", "Closed Socket")
     }
 
     fun uploadUID(uid: String){
         Log.i("Uploadutil", String.format("Uploading UID %s", uid))
-        if(serverStream != null && serverSocket != null){
-            val serverStreamWrite = OutputStreamWriter(serverStream, StandardCharsets.UTF_8)
-            serverStreamWrite.write(uid)
-        }
+        serverSocket?.send(uid)
         Log.i("Uploadutil", String.format("Uploaded UID %s", uid))
     }
 
     fun uploadFile(fileName: String) {
         Log.i("Uploadutil", String.format("Uploading File %s", fileName))
-        if(serverSocket != null && serverStream != null){
-            val fileInputStream = DataInputStream(FileInputStream(File(fileName)))
-            val fileOutputStream = DataOutputStream(serverStream)
-
-            var buffer = ByteArray(1024)
-            while(fileInputStream.read(buffer) > 0){
-                fileOutputStream.write(buffer)
-                fileOutputStream.flush()
+        val file = File(fileName)
+        val fileInputStream = FileInputStream(file)
+        val buffer = ByteArray(file.length().toInt())
+        try {
+            fileInputStream.read(buffer)
+            val byteString = buffer.toByteString(0, buffer.size)
+            serverSocket?.send(byteString)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            try {
+                fileInputStream.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
-
-            fileOutputStream.close()
-            Log.i("Uploadutil", String.format("Uploaded File %s", fileName))
         }
+        Log.i("Uploadutil", String.format("Uploaded File %s", fileName))
     }
 }
