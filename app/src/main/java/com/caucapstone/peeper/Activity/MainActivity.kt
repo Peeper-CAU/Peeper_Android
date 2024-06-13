76ae4cc2f69bf1c5efcacb8e11ed3cf6a059806a
yymin1022 <yymin1022@gmail.com>
2024-06-14 03:37:05 +0900

    Update MainActivity : Added Record-Save-Upload Logic

--- app/src/main/java/com/caucapstone/peeper/Activity/MainActivity.kt
+++ app/src/main/java/com/caucapstone/peeper/Activity/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.caucapstone.peeper.Activity
 
 import android.Manifest
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
+import android.media.AudioFormat
+import android.media.AudioRecord
+import android.media.MediaRecorder
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -14,7 +17,6 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.caucapstone.peeper.R
-import com.caucapstone.peeper.Service.BluetoothService
 import com.caucapstone.peeper.Util.FileUtil
 import com.caucapstone.peeper.Util.UploadUtil
 import com.google.android.material.button.MaterialButton
@@ -22,9 +24,14 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.IOException
+
 
 class MainActivity : AppCompatActivity() {
     private var fileName = ""
+    private var isRecording = false
+
+    val testUID = "TEST_UID"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,34 +61,79 @@ class MainActivity : AppCompatActivity() {
 
     private val btnListener = View.OnClickListener { btn ->
         when(btn.id) {
-            R.id.main_btn_start -> startService(Intent(this, BluetoothService::class.java))
-            R.id.main_btn_stop -> stopService(Intent(this, BluetoothService::class.java))
+            R.id.main_btn_start -> startRecord()
+            R.id.main_btn_stop -> stopRecord()
             R.id.main_btn_test_file -> testFile()
             R.id.main_btn_test_upload -> testUpload()
         }
     }
 
+    @SuppressLint("MissingPermission")
+    private fun startRecord() {
+        val recordChannel = AudioFormat.CHANNEL_IN_MONO
+        val recordEncoding = AudioFormat.ENCODING_PCM_8BIT
+        val recordSampleRate = 44100
+
+        val recordByteSize = AudioRecord.getMinBufferSize(recordSampleRate, recordChannel, recordEncoding)
+        val audioRecord = AudioRecord(
+            MediaRecorder.AudioSource.MIC,
+            recordSampleRate,
+            recordChannel,
+            recordEncoding,
+            recordByteSize
+        )
+
+        FileUtil.initFile(testUID, applicationContext)
+
+        val audioBuffer = ByteArray(recordByteSize)
+        val coroutineScope = CoroutineScope(Dispatchers.Main)
+        coroutineScope.launch {
+            withContext(Dispatchers.IO) {
+                audioRecord.startRecording()
+                isRecording = true
+                while(isRecording) {
+                    val readResult = audioRecord.read(audioBuffer, 0, recordByteSize)
+                    if(readResult == AudioRecord.ERROR_INVALID_OPERATION ||
+                        readResult == AudioRecord.ERROR_BAD_VALUE)
+                        continue
+                    try {
+                        FileUtil.writeFile(audioBuffer, recordByteSize)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
+                audioRecord.stop()
+                fileName = FileUtil.closeFile(testUID, recordByteSize)
+
+                UploadUtil.uploadUID(testUID)
+                UploadUtil.uploadFile(fileName)
+            }
+        }
+    }
+
+    private fun stopRecord() {
+        isRecording = false
+    }
+
     private fun testFile() {
         val testBytes = byteArrayOf(0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1)
-        val testUID = "TEST_UID"
         Log.d("Util Test", "File Test Started!!")
         Log.d("Util Test", "Initializing File!!")
         FileUtil.initFile(testUID, applicationContext)
         Log.d("Util Test", "Initialized File!!")
 
         Log.d("Util Test", "Writing Bytes to File!!")
-        FileUtil.appendData(testBytes)
+//        FileUtil.appendData(testBytes)
         Log.d("Util Test", "Wrote to File!!")
 
         Log.d("Util Test", "Closing File!!")
-        fileName = FileUtil.closeFile(testUID)
+//        fileName = FileUtil.closeFile(testUID)
         Log.d("Util Test", "Closed File!!")
         Log.d("Util Test", "File Test Done!!")
     }
 
     private fun testUpload() {
         val coroutineScope = CoroutineScope(Dispatchers.Main)
-        val testUID = "TEST_UID"
         Log.d("Util Test", "Upload Test Started!!")
 
         coroutineScope.launch {
@@ -124,5 +176,5 @@ class MainActivity : AppCompatActivity() {
                     allPermissionsGranted = false
                 }
             }
-        }
+    }
 }
